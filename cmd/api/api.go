811151dbff5cff7e898c35816320cf9036575007
devjunaeid <junaeid.md48@gmail.com>
2024-04-21 14:06:59 +0600

    Implemented User routes/route group.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,8 +1,14 @@
 package api
 
 import (
+	"github.com/devjunaeid/cms-admin-backend/routes"
 	"github.com/gofiber/fiber/v3"
 )
+
+type greeting struct {
+	Message  string `json:"message"`
+	Greeting string `json:"greeting"`
+}
 
 func CreateAPI() {
 	app := fiber.New(fiber.Config{
@@ -10,8 +16,15 @@ func CreateAPI() {
 	})
 
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, Juaneid!")
+		c.Accepts("application/json")
+		data := greeting{
+			Message:  "Hello There",
+			Greeting: "Welcome to Fiber!",
+		}
+		return c.JSON(data)
 	})
-
+	// User Route Group.
+	userHandler := app.Group("/user")
+	routes.CeateRoute(userHandler)
 	app.Listen(":3000")
 }
