aa453637e06a46582fcf4c267bbb69a46fd3b0f7
devjunaeid <junaeid.md48@gmail.com>
2024-04-27 09:19:24 +0600

    DB init and pass db to the api.

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"github.com/devjunaeid/cms-admin-backend/cmd/api"
+	"github.com/devjunaeid/cms-admin-backend/config"
+	"github.com/devjunaeid/cms-admin-backend/db"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	api.CreateAPI()
+
+	// Initializing Database.
+	database := db.InitDB(config.Env.PsqlConnectionUrl)
+	if database == nil {
+		log.Fatal().Msg("Failed to initalized Database! Exiting!!")
+		return
+	}
+
+	// Initializing API.
+	api.CreateAPI(database)
 }
