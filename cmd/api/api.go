a4a366ebb0545ef9c227c7a48601f78088083ac3
devjunaeid <junaeid.md48@gmail.com>
2024-04-27 09:02:24 +0600

    Added: Database and route template.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/devjunaeid/cms-admin-backend/routes"
 	"github.com/gofiber/fiber/v3"
+	"gorm.io/gorm"
 )
 
 type greeting struct {
@@ -10,7 +11,7 @@ type greeting struct {
 	Greeting string `json:"greeting"`
 }
 
-func CreateAPI() {
+func CreateAPI(db *gorm.DB) {
 	app := fiber.New(fiber.Config{
 		AppName: "Synafeia CMS Backend",
 	})
@@ -18,13 +19,15 @@ func CreateAPI() {
 	app.Get("/", func(c fiber.Ctx) error {
 		c.Accepts("application/json")
 		data := greeting{
-			Message:  "Hello There",
-			Greeting: "Welcome to Fiber!",
+			Message:  "This for admin personel only!!",
+			Greeting: "Welcome to Synafeia-backend!",
 		}
 		return c.JSON(data)
 	})
 	// User Route Group.
 	userHandler := app.Group("/user")
-	routes.CeateRoute(userHandler)
+	userRoute := routes.InitUser(userHandler, db)
+	userRoute.CreateRoute()
+
 	app.Listen(":3000")
 }
