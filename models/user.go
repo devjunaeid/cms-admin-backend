0a8ab71a42251e8666cf36b5904e461c00b2aeac
devjunaeid <junaeid.md48@gmail.com>
2024-04-30 10:14:17 +0600

    Patched: User Model json binding , UserResponse and RegisterPayload json mapping.

--- models/user.go
+++ models/user.go
@@ -1,16 +1,21 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
-type User struct {
+type Users struct {
 	gorm.Model
-	ID     uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
-	Name   string
-	Email  string
-	Role   string `gorm:"default:user"`
-	Img    string
-	Active bool `gorm:"default:false"`
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	Name      string
+	Email     string
+	Password  string
+	Role      string `gorm:"default:user"`
+	Img       string
+	Active    bool      `gorm:"default:false"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
--- types/user.go
+++ types/user.go
@@ -1,18 +1,21 @@
 package types
 
-type User struct {
+type UserResponse struct {
+	ID        string `json:"id"`
 	Email     string `json:"email"`
-	Password  string `json:"password"`
 	Name      string `json:"name"`
-	Role      string `json:"role" default:"user"`
+	Role      string `json:"role"`
+	Active    bool   `json:"active"`
+	Img       string `json:"img"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 }
 
 type RegisterPayload struct {
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
+	Password string `json:"password" validate:"required"`
+	Name     string `json:"name" validate:"required,alphaunicode,min=2,max=30"`
+	Img      string `json:"img"`
 }
 
 type LoginPayload struct {
