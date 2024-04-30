c200781b9a13cf57a93f237f131641536a92148e
devjunaeid <junaeid.md48@gmail.com>
2024-04-30 10:21:36 +0600

    Added: Success Response maker funciton.

--- utils/utils.go
+++ utils/utils.go
@@ -4,9 +4,18 @@ import (
 	"github.com/devjunaeid/cms-admin-backend/types"
 )
 
+// Create Error Response JSON.
 func CreateErrorRes(msg string, status int) types.ErrorDefault {
 	return types.ErrorDefault{
 		Status: status,
 		Err:    msg,
+	}
+}
+
+// Create Operation Success Response JSON.
+func CreateSuccessRes(msg string, status int) types.SuccessDefault {
+	return types.SuccessDefault{
+		Status: status,
+		Msg:    msg,
 	}
 }
