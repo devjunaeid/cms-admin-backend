fbadc531394257f4ba426acdeb79a481f5ee9038
junaeid <junaeid.md48@gmail.com>
2024-05-17 21:53:41 +0600

    Added: Patch user route and route handler & Fixed: User delete user not found response.

--- routes/user.go
+++ routes/user.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/devjunaeid/cms-admin-backend/models"
 	"github.com/devjunaeid/cms-admin-backend/types"
 	"github.com/devjunaeid/cms-admin-backend/utils"
@@ -31,6 +34,8 @@ func (ur *UserRoute) CreateRoute() {
 	ur.route.Get("/users", ur.getUsers)
 	ur.route.Get("/user/:id", ur.getUser)
 
+	// Update User Role.
+	ur.route.Patch("/user/:id", ur.patchUsers)
 	// Delete User.
 	ur.route.Delete("/user/:id", ur.deleteUser)
 }
@@ -67,10 +72,9 @@ func (ur *UserRoute) getUser(c fiber.Ctx) error {
 // Delete User.
 func (ur *UserRoute) deleteUser(c fiber.Ctx) error {
 	userID := c.Params("id")
-
 	dbRes := ur.db.Where("id=?", userID).Delete(&models.Users{})
-
-	if dbRes.Error == gorm.ErrRecordNotFound {
+	fmt.Println(dbRes.Error)
+	if dbRes.RowsAffected < 1 {
 		res := utils.CreateErrorRes("No record found to delete!!", fiber.ErrBadRequest.Code)
 		return c.JSON(res)
 	} else if dbRes.Error != nil {
@@ -79,5 +83,37 @@ func (ur *UserRoute) deleteUser(c fiber.Ctx) error {
 	}
 
 	res := utils.CreateSuccessRes("Deleted!!", fiber.StatusAccepted)
+	return c.JSON(res)
+}
+
+// Patch User.
+func (ur *UserRoute) patchUsers(c fiber.Ctx) error {
+	userID := c.Params("id")
+	if c.Request().Body() == nil {
+		res := utils.CreateErrorRes("Bad Request", fiber.ErrBadRequest.Code)
+		return c.JSON(res)
+	}
+
+	payload := new(types.UpdateUserRolePayload)
+	c.Bind().Body(payload)
+	err := validator.Struct(payload)
+	if err != nil {
+		log.Printf("Error updating user, error: %v", err.Error())
+		res := utils.CreateErrorRes("Failed to update user", fiber.ErrBadRequest.Code)
+		return c.JSON(res)
+	}
+
+	// Check If the user already registerd.
+	var user models.Users
+	dbRes := ur.db.Model(&user).Where("id=?", userID).Update("role", payload.Role)
+	if dbRes.RowsAffected < 1 {
+		res := utils.CreateErrorRes("No record found to update!!", fiber.ErrBadRequest.Code)
+		return c.JSON(res)
+	} else if dbRes.Error != nil {
+		res := utils.CreateErrorRes("Faild to update!!", fiber.ErrInternalServerError.Code)
+		return c.JSON(res)
+	}
+
+	res := utils.CreateSuccessRes("Role updated!", fiber.StatusCreated)
 	return c.JSON(res)
 }
